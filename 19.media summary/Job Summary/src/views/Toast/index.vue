e38c470788410953fcf17dfb3ee6df503ced48b3
huangsl <zjx1020645@163.com>
2019-04-29 11:24:22 +0800

    add

--- 19.media summary/Job Summary/src/views/Toast/index.vue
+++ 19.media summary/Job Summary/src/views/Toast/index.vue
@@ -52,7 +52,7 @@ export default {
     font-family: '微软雅黑','Helvetica Neue',Helvetica,Arial,sans-serif;
     box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12);;
     cursor: pointer;
-    text-decoration: none;
+    text-decoration: none;#3383da
     transition: 0.12s ease-in;
     border: 0;
     outline: 0;
