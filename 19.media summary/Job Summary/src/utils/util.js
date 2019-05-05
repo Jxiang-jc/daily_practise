e5abd014590b24569bb01641265859c5033596b9
huangsl <zjx1020645@163.com>
2019-05-06 00:03:34 +0800

    添加使用方法

--- 19.media summary/Job Summary/src/utils/util.js
+++ 19.media summary/Job Summary/src/utils/util.js
@@ -1,4 +1,12 @@
 var exp = {
+  /*
+    组件中引入
+    import exp from '~/util'
+    在methods中声明一个方法
+    methods: {
+      example: exp.debounce(function(){}, 800)
+    }
+  */
   // 防抖函数， 让某个函数在上一次执行后， 满足等待某个时间内不再触发此函数后再执行， 而在这个等待时间内再次触发此函数，等待时间会重新计算
   debounce (func, wait, immediate) {
     var timeout, args, context, timestamp, result
