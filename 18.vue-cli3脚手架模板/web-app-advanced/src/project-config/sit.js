e4b40f3cc7d1f38415cdcae0655cbe37d678259c
Jxiang-jc <zjx1020645@163.com>
2019-06-04 15:11:38 +0800

    axios 二次封装

---
+++ 18.vue-cli3脚手架模板/web-app-advanced/src/project-config/sit.js
@@ -0,0 +1,10 @@
+/** @format */
+
+'use strict';
+
+window.PROJECT_CONFIG = {
+    // baseUrl: 'http://10.16.80.232:7004/mobile-web/public-access',
+    baseUrl: '',
+    appKey: '',
+    widgetKey: ''
+};
